da662b53e7c845f6e4df94cac13020ae5fd2a16f
kimdonghyeon <sh2115521@naver.com>
2022-05-11 15:08:23 +0900

    로그인구현

--- src/login.js
+++ src/login.js
@@ -1,15 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
 import {Header} from './laydout/header';
 import {Footer} from './laydout/footer';
-function Login(){
+import {BrowserRouter, Routes, Route, Link, useParams} from 'react-router-dom';
+import './login.css';
+
+
+function Login(props){
+    const [state, setState] = useState({
+        email: "",
+        password: "",
+    });
+    console.log("렌더링");
+    const handleChange = (e) => {
+        const { id, value } = e.target;
+        setState((prevState) => ({
+            ...prevState,
+            [id]: value,
+        }));
+    };
 
     return(
+
         <div>
             <Header></Header>
-            <h2> 로그인 페이지</h2>
-            <Footer/>
-        </div>
-    )
-}
+            <div className="gap"></div>
+            <h2><center><strong>문앞에</strong></center></h2>
+            <div className="user_btn"><button className="user_bt"> 개인 로그인</button>
+                <div className="company_btn"><button className="company_bt"> 기업 로그인</button></div>
+            </div>
 
+            <dd className="user_id">
+                <input
+                    type="email"
+                    id="email"
+                    placeholder="아이디"
+                    value={state.email}
+                    onChange={handleChange}/>
+
+            </dd>
+
+            <dd className="user_pw">
+                <input
+                    type="password"
+                    id="password"
+                    placeholder="비밀번호"
+                    value={state.password}
+                    onChange={handleChange}
+                />
+            </dd>
+
+            <div className="login_config"><button className="login_configbt"> 로그인</button></div>
+            <div className="gap"></div>
+            <div className="findid"><button className="findid_bt"> 아이디찾기</button>
+                <div className="findpw"><button className="findpw_bt"> 비밀번호찾기</button>
+                    <div className="signup"><button className="signup_bt"> 회원가입</button></div>
+                </div>
+            </div>
+            <div className="'loginnn'"><Link className="category_sub_link" to="/">로그인</Link></div>
+            <div className="gap"></div>
+            <Footer></Footer>
+        </div>
+
+    );
+
+
+    function company(){
+//라우터 링크 연결 및 로그인 누르면 메인 페이지로 가게
+        return(
+            <div>
+                <Header></Header>
+                <h2> aa</h2>
+                <ul>
+                    <li><Link to="/login/user"> 유저</Link></li>
+                    <li><Link to="/login/company">기업</Link></li>
+                </ul>
+
+                {/*<Routes>*/}
+                {/*    <Route path="/:login_id/*" element={login}></Route>*/}
+                {/*</Routes>*/}
+
+                <Footer/>
+
+            </div>
+        )
+    }
+}
 export default Login;
--- src/mypage.js
+++ src/mypage.js
@@ -1,24 +1,216 @@
-import React from "react";
+import React, {useState} from "react";
 import {Header} from './laydout/header';
 import {Footer} from './laydout/footer';
-import {BrowserRouter, Routes, Route, Link, useParams} from 'react-router-dom';
+import {BrowserRouter, Routes, Route, Link, useParams } from 'react-router-dom';
 import {Row, Tab, Col, Nav} from "react-bootstrap";
 import './mypage.css';
 
-const contents = [
-    {id:1, title:'user', description:<User/>},
-    {id:2, title:'company', description:<Company/>},
+
+//기업페이지
+function Company_profile(){
+    return(
+        <div>
+            <h2 className="company_profile_h2"> 기업 프로필</h2>
+            <hr/>
+            <dl>
+                <dt className="user_profile_dt"><label>기업명</label></dt>
+                <dd className="user_edit_dd"><input type="text" placeholder="기업명"></input>
+                    <button type="button" className="user_edit_btn"> 변경 </button></dd>
+                <dt className="user_profile_dt"><label>이메일</label></dt>
+                <dd className="user_edit_dd"><input type="text" placeholder="이메일"></input>
+                    <button type="button" className="user_edit_btn"> 변경 </button></dd>
+                <dt className="user_profile_dt"><label>주소</label></dt>
+                <dd className="user_edit_dd"><input type="text" placeholder="주소"></input>
+                    <button type="button" className="user_edit_btn"> 변경 </button></dd>
+                <dt className="user_profile_dt"><label>상담번호</label></dt>
+                <dd className="user_edit_dd"><input type="text" placeholder="상담번호"></input>
+                    <button type="button" className="user_edit_btn"> 변경 </button></dd>
+            </dl>
+        </div>
+    )
+}
+
+function Company_manage(){
+    return(
+        <div>
+            <h2 className="company_profile_h2"> 등록 상품 관리</h2>
+            <hr/>
+            <div className="product_container">
+                <div className="product">
+                    <div className="product_img_div"><img src={require("./img/product_img.png")} className="product_img"/></div>
+                    <h5 className="product_title"> 상품 제목</h5>
+                    <p className="product_des"> 상품 내용 요약</p>
+                    <div className="product_mon"> 월 : 15,000￦</div>
+                    <div className="product_link_div"><button className="product_th_btn"> 수정하기 </button></div>
+                </div>
+                <div className="product">
+                    <div className="product_img_div"><img src={require("./img/product_img.png")} className="product_img"/></div>
+                    <h5 className="product_title"> 상품 제목</h5>
+                    <p className="product_des"> 상품 내용 요약</p>
+                    <div className="product_mon"> 월 : 15,000￦</div>
+                    <div className="product_link_div"><button className="product_th_btn"> 수정하기 </button></div>
+                </div>
+            </div>
+            <div className="company_product_enroll_div"><button className="company_product_enroll_btn">상품 등록하러 가기</button></div>
+        </div>
+    )
+}
+
+function Deliver_table(props){
+    const table = [];
+
+    for(let i=0; i<props.deliver_list.length; i++){
+        let t = props.deliver_list[i];
+        table.push(<li key={t.title}>
+            <div>{t.title}</div>
+            <div>{t.body}</div>
+        </li>)
+    }
+
+    return(
+        <div>
+            {table}
+        </div>
+    )
+}
+
+function Company_deliver(){
+
+    const [mode, setMode] = useState('NORMAL');
+
+    let deliver_li = null;
+
+    const [deliver_list,setDeliver_list] = useState([
+        {title:"운송장번호", body:"123456"}
+    ])
+
+    console.log("실행됨");
+    if(mode === 'NORMAL'){
+        console.log("nomal 실행됨");
+        deliver_li = <Deliver_table deliver_list={deliver_list}></Deliver_table>
+    }else if(mode === 'CREATE'){
+        console.log("create 실행됨");
+        deliver_li = <Deliver_table deliver_list={deliver_list}></Deliver_table>
+    }
+
+    return(
+        <div>
+            <h2 className="company_profile_h2"> 배송 조회</h2>
+            <hr/>
+            <form onSubmit={event=>{
+                event.preventDefault();
+                const deliver_num = event.target.deliver_number.value;
+                const new_deliver_num = {title : "운송장번호", body:deliver_num};
+                const new_deliver_list = [...deliver_list];
+                new_deliver_list.push(new_deliver_num);
+                setDeliver_list(new_deliver_list);
+                setMode("CREATE");
+            }}>
+                <dl>
+                    <dt className="user_profile_dt"><label>운송장 등록</label></dt>
+                    <dd className="user_edit_dd"><input type="text"  name="deliver_number" placeholder="deliver_number"></input>
+                        <button type="submit" className="user_edit_btn"> 등록 </button></dd>
+                </dl>
+            </form>
+            <hr/>
+                {deliver_li}
+            <hr/>
+        </div>
+    )
+}
+
+function Company_product(){
+
+    const [fileimage, setFileimage] = useState();
+
+    const saveFileImage = (event)=>{
+        setFileimage(URL.createObjectURL(event.target.files[0]));
+    }
+
+    const deleteFileImage = (event) => {
+        URL.revokeObjectURL(fileimage);
+        setFileimage("");
+    }
+    return(
+        <div>
+            <h2 className="company_profile_h2"> 상품 등록 페이지</h2>
+            <hr/>
+            <form>
+                <p> 대표 사진 업로드 </p>
+                <div>{fileimage && (<img alt="preview" src={fileimage}/>)}
+                <button onClick={() => deleteFileImage()}> 삭제 </button>
+                </div>
+                <input type="file" accept="image/*" onChange={saveFileImage}></input>
+            <p> 상평명 </p>
+            <p> 카테고리설정 </p>
+            <p> 월/가격 및 옵션 </p>
+            <p> 상담번호 </p>
+            <p> 사업장 주소 </p>
+            <p> 상품 상세 설명 </p>
+            </form>
+        </div>
+    )
+}
+
+const company_content=[
+    {id:1, title:'profile', description:<Company_profile/>},
+    {id:2, title:'manage', description:<Company_manage/>},
+    {id:1, title:'deliver', description:<Company_deliver/>},
+    {id:2, title:'product', description:<Company_product/>},
 ]
 
 function Company(){
 
     return(
         <div>
-            <h2> 기업 마이페이지</h2>
+            <div><h2 className="User_title"> 기업 마이페이지</h2></div>
+
+            <div className="user_container">
+
+                <div className="user_navbar">
+                    <ul className="user_navbar_list">
+                        <li className="user_navbar_li"><Link className="user_navbar_link" to="/mypage/company/profile"> 기업 프로필</Link></li>
+                        <li className="user_navbar_li"><Link className="user_navbar_link" to="/mypage/company/manage"> 상품 관리</Link></li>
+                        <li className="user_navbar_li"><Link className="user_navbar_link" to="/mypage/company/deliver"> 배송 조회</Link></li>
+                        <li className="user_navbar_li"><Link className="user_navbar_link" to="/mypage/company/product"> 상품 등록</Link></li>
+                    </ul>
+                </div>
+
+                <div className="user_content">
+                    <Routes>
+                        <Route path="/:company_nav" element={< Company_content/>}></Route>
+                    </Routes>
+                </div>
+            </div>
         </div>
     )
 }
 
+function Company_content(){
+    var params = useParams();
+    var user_nav = params.company_nav;
+    var selected_category ={
+        title : 'sorry',
+        description : "No Script"
+    }
+    console.log("real national main : ",selected_category);
+    for(let i = 0 ; i < company_content.length ; i++){
+        if(company_content[i].title === user_nav){
+            selected_category = company_content[i];
+            break;
+        }
+    }
+
+    return(
+        <div>
+            <h2>{selected_category.description}</h2>
+        </div>
+
+
+    )
+}
+
+//유저페이지
 function User_profile(){
 
     return(
@@ -208,12 +400,16 @@ function User(){
                     </Routes>
                 </div>
             </div>
-
-
-
         </div>
     )
 }
+
+
+//메인 마이페이지
+const contents = [
+    {id:1, title:'user', description:<User/>},
+    {id:2, title:'company', description:<Company/>},
+]
 
 function Mypage_content(){
     var params = useParams();
