75377ced764b880f39f5fe74bef2082043486909
sonilgook <thsdlfrnr@naver.com>
2022-06-04 22:15:27 +0900

    Changes

--- src/laydout/content.js
+++ src/laydout/content.js
@@ -3,13 +3,12 @@ import React from "react";
 import {NavLink} from 'react-router-dom';
 
 export function Content(){
+
     return(
         <main className="content" style={{backgroundColor:'white'}}>
             <div>
-
                  <div>
                     <NavLink to="/category/main">
-
                     <div className="arrangement">
                     <video autoPlay loop muted >
                     <source src={require("../img/video_123.mp4")} type="video/mp4"/>
